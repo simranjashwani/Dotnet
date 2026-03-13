e75164c40bced08b0e99860d44fe57e04c904355
simranjashwani <simranjashwani46@gmail.com>
2026-03-13 14:58:58 +0530

    Light bulb example

--- Day25/commandpattern/Program.cs
+++ Day25/commandpattern/Program.cs
@@ -1,4 +1,25 @@
 ﻿
+// ICommand createCustomerCommand = new CreateCustomerCommand();
+// createCustomerCommand.Execute();
+
+// //solid 
+// CreateCustomer();
+// void CreateCustomer()
+// {
+//     Console.WriteLine("creating customer directly");
+// }
+
+// public interface ICommand
+// {
+//     void Execute();
+// }
+// public class CreateCustomerCommand : ICommand
+// {
+//     public void Execute()
+//     {
+//         Console.WriteLine("create customer ");
+//     }
+// }
 
 Light light = new Light();
 // light.On();
